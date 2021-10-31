c3b8b5073907a92b15a62f6f53816668520c0542
ChakshuGautam <chaks.gautam@gmail.com>
2021-11-01 02:41:34 +0530

    Remove files not needed

--- src/pdf/settings.py
+++ src/pdf/settings.py
@@ -13,7 +13,7 @@ from datetime import timedelta
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv('.env.dev')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
