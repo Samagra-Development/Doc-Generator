c1d2f1124e13660130ce6d0cd0078e34b1dad3e9
ChakshuGautam <chaks.gautam@gmail.com>
2021-10-31 22:05:35 +0530

    Fix: Remove common docs

--- src/pdf/urls.py
+++ src/pdf/urls.py
@@ -1,18 +1,3 @@
-"""pdf URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
@@ -22,6 +7,5 @@ urlpatterns = [
     path(r'ht/', include('health_check.urls')),
     path('grappelli/', include('grappelli.urls')),  # Admin grappelli URLS
     path('admin/', admin.site.urls),
-
     path('test-page/', current_datetime),
 ]
