cc68de0c5e4b44cc077e95f7abd9866141bebe22
Radhay Anand <radhay@samagragovernance.in>
2022-06-01 02:51:34 +0530

    fix: docker-compose and gitpod configs

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cp .env.sample .env
+export DATABASE_URL="postgresql://postgresql:yoursupersecret@localhost:10021/templaterdb?schema=public"
 docker-compose -f docker-compose-gitpod.yml up -d --build
