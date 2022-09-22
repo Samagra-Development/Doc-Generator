85bf329f1e547980fa27e2a4010d15e53ef1dd22
yuvraj <yuvrajsab@protonmail.com>
2022-09-22 11:57:05 +0530

    add wait in deploy.sh file and change default wkhtmltopdf path in env

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,6 @@ docker-compose -f docker-compose-gitpod.yml up -d --build
 
 docker exec doc-generator-web-1 python3 manage.py makemigrations
 docker exec doc-generator-web-1 python3 manage.py migrate
+
+echo "wait for 30 sec to let container start"
+sleep 30 # let container start
