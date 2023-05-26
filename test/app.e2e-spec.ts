b572ca795a53a12ac3770acbae99533381038ac7
Varun Singh <varun7singh10@gmail.com>
2023-05-26 20:32:23 +0530

    fix(api): fixed the the app.e2e-spec.ts by mocking the PrismaService

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,21 +2,31 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
+  const mockPrismaService = {
+    $connect: jest.fn().mockImplementation(async () => {
+      return Promise.resolve();
+    }),
+  };
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-      // TODO: mock prisma service
-    }).compile();
+      providers: [PrismaService],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockPrismaService)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/api').expect('Doc Generator');
+    return request(app.getHttpServer()).get('/').expect('Doc Generator');
   });
 });
