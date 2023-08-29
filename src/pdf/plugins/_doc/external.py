d81c7a0ad902791eaf94db7fc354975a160ccc31
Yuvraj Sablania <27115069+yuvrajsab@users.noreply.github.com>
2023-08-29 16:59:53 +0530

    remove hardcoded urls and move it to env (#170)
    * remove hardcoded urls and move it to env
    
    * fix minio bucket ssl issue

--- src/pdf/plugins/_doc/external.py
+++ src/pdf/plugins/_doc/external.py
@@ -51,10 +51,12 @@ class DOCXPlugin(PDFPlugin):
             drive_file_loc = f'pdf/drivefiles/{self.token}.docx'
             if self.uploader == "minio":
                 host = self.user_config["MINIO_HOST"]
+                isHttps = self.user_config.get("MINIO_IS_HTTPS", False)
                 access_key = self.user_config["MINIO_ACCESS_KEY"]
                 secret_key = self.user_config["MINIO_SECRET_KEY"]
                 bucket_name = self.user_config["MINIO_BUCKET_NAME"]
-                uploader = MinioUploader(host, access_key, secret_key, bucket_name)
+                cred_expiry_duration = self.user_config.get("MINIO_CREDENTIAL_EXPIRY_DURATION")
+                uploader = MinioUploader(host, access_key, secret_key, bucket_name, cred_expiry_duration, isHttps)
                 error_code, error_msg, final_data = uploader.put(f'{self.token}.docx', f'{self.token}.docx', None)
                 if error_code is None:
                     if os.path.exists(drive_file_loc):
