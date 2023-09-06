0e01088a631622bee36b238685e8e3ba5ee56d16
Swatishree Mahapatra <swatishree1508@gmail.com>
2023-09-07 00:58:22 +0530

    add create batch, fetch batches, fetch templates API integration

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -19,3 +19,52 @@ export const generateRender = async (templateType, templateInput, data) => {
     throw error;
   }
 };
+
+export const createBatch = async (templateType, templateID, payload) => {
+  try {
+    const requestBody = {
+      templateType: templateType,
+      templateID: templateID,
+      payload: payload,
+    };
+
+    const response = await axios.post(
+      `${API_BASE_URL}/api/generate/batches`,
+      requestBody,
+    );
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchBatches = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/api/generate/batches`);
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      console.error('Failed to fetch batches:', response.status);
+      return [];
+    }
+  } catch (error) {
+    console.error('Error fetching batches:', error);
+    return [];
+  }
+};
+
+export const fetchTemplates = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/api/template`);
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      console.error('Failed to fetch templates:', response.status);
+      return [];
+    }
+  } catch (error) {
+    console.error('Error fetching templates:', error);
+    return [];
+  }
+};
